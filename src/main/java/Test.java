219a18170cc9aa42d4fc0f7069417d761a45bdaf
yuanjianq <718932905@qq.com>
2018-10-10 16:58:26 +0800

    初始导入

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @author yuanjianqiang
+ * @email yuanjianqiang@dafy.com
+ * @date 2018/10/10 16:57
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Word~");
+    }
+}
