2f422a8bffc847dbc6b07facba6f25a90f81d2a4
yuanjianqiang <yuanjianqiang@youzan.com>
2019-04-04 15:55:19 +0800

    test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -13,5 +13,9 @@ public class Test {
         System.out.println("Hello Word~");
         System.out.println("Hello Word~sdvsvsssssvsss");
         System.out.println("Hello Wordadadadasdaadad~");
+        System.out.println("Hello Wordadadadasdaadad~");
+        System.out.println("Hello Wordadadadasdaadad~");
+        System.out.println("Hello Wordadadadasdaadad~");
+        System.out.println("Hello Wordadadadasdaadad~");
     }
 }
