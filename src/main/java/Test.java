f78f0035095f2cb738b5f78b279fe49adfa877b6
yuanjianqiang <yuanjianqiang@youzan.com>
2019-04-04 16:13:33 +0800

    seneef-brance

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -17,5 +17,6 @@ public class Test {
         System.out.println("Hello Wordadadadasdaadad~");
         System.out.println("Hello Wordadadadasdaadad~");
         System.out.println("Hello Wordadadadasdaadad~");
+        System.out.println("Hello second-brance~");
     }
 }
